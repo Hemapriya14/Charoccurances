db288b58b34e11cab92ba0be64be29fa3fd3b755
Hemapriya14 <99642541+Hemapriya14@users.noreply.github.com>
2022-02-15 05:52:12 -0800

    Add files via upload

---
+++ CharacterOcuurance.java
@@ -0,0 +1,12 @@
+package week2.day1;
+
+public class CharacterOcuurance {
+
+	public static void main(String[] args) {
+		String input="Welcome to Chennai";
+		String newstring=input.replace("e","");
+		System.out.println(input.length()-newstring.length());
+
+	}
+
+}
